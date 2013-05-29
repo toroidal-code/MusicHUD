14e0470658851b4d9627b35aacfb6093fd7e3c11
Katherine Whitlock <toroidalcode@gmail.com>
2013-05-28 21:16:26 -0400

    Initial Commit

---
+++ MusicHUD/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MusicHUD
+//
+//  Created by Katherine Whitlock on 5/28/13.
+//  Copyright (c) 2013 Katherine Whitlock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
